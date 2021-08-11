1f194efc59cbce2af444dd7558360fc1370e3b14
Christopher Kellum <christopherk379@gmail.com>
2021-08-11 19:09:48 -0400

    adding apropriate tests

---
+++ test/object_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_of_app/objects/game.dart';
+import 'package:flutter_of_app/objects/movie.dart';
+import 'package:flutter_of_app/objects/music.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Movie required data properly', () {
+    final String title = "Slepless in seatle";
+    final Movie movie = Movie(title: title);
+
+    expect(movie.title, title);
+  });
+
+  test('Music required data properly', () {
+    final String title = "Black album";
+    final Music music = Music(albumTitle: title);
+    expect(music.albumTitle, title);
+  });
+
+  test('Games required data properly', () {
+    final String title = "Dungeons";
+    final Game game = Game(title: title);
+    expect(game.title, title);
+  });
+}
